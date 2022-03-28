ffc75ea8f5517f844cc1c9cccc9df440fb6fd8e5
JUYOUNG KIM <wndud2274@gmail.com>
2022-03-29 00:24:53 +0900

    refactor: 함수 분리

--- baseball.js
+++ baseball.js
@@ -1,19 +1,7 @@
 const readline = require("readline");
+const { gamePlay, randomNum } = require("./function");
 
-//랜덤한 3자리 숫자 생성(정답 생성)
-const randomNum = () => {
-  let array = [];
-  while (array.length < 3) {
-    //1 ~ 9 사이 숫자 생성
-    let num = Math.floor((Math.random() + 0.1) * 9);
-
-    // 중복된 수 확인
-    if (!array.includes(num)) {
-      array.push(num);
-    }
-  }
-  return array;
-};
+const randomArray = randomNum();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,13 +9,12 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (input) => {
-  //입력값이 3자리 숫자가 아닐 때
+  console.log(`# 숫자를 입력해주세요 : ${input}`);
   if (input.length !== 3) {
     console.log("3자리 숫자를 입력해주세요");
   } else {
-    const array = randomNum();
-    // console.log(input); //test
-    console.log(array); //test
+    gamePlay(input, randomArray);
+
     //   rl.close(); //test
   }
 });
