7600f8e85945955994394f7655a945d4e4129200
JUYOUNG KIM <wndud2274@gmail.com>
2022-03-29 09:44:45 +0900

    refactor: gamePlay 함수 리팩토링

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-//1. 랜덤한 3자리 숫자 생성(정답 생성)
+//랜덤한 3자리 숫자 생성(정답 생성)
 const randomNum = () => {
   let array = [];
   while (array.length < 3) {
@@ -13,12 +13,9 @@ const randomNum = () => {
 
 //세자리수 입력 (게임 시작)
 const gamePlay = (input, randomArray) => {
-  if (randomArray.join("") === input) {
-    return false;
-  } else {
-    inputCheck(input, randomArray);
-    return true;
-  }
+  return randomArray.join("") === input
+    ? false
+    : inputCheck(input, randomArray);
 };
 
 //스트라이크, 볼, 낫싱
@@ -27,17 +24,15 @@ const inputCheck = (input, randomArray) => {
   let strike = 0;
   let ball = 0;
 
-  for (let i = 0; i < 3; i++) {
-    if (inputArray[i] === randomArray[i]) {
-      strike++;
-    } else if (randomArray.includes(inputArray[i])) {
-      ball++;
-    }
-  }
-  if (!strike && !ball) {
-    return console.log("낫싱");
-  }
-  return console.log(`${strike} 스트라이크, ${ball} 볼 \n`);
+  inputArray.map((v, i) => {
+    v === randomArray[i] ? strike++ : randomArray.includes(v) ? ball++ : "";
+  });
+
+  !strike && !ball
+    ? console.log("낫싱")
+    : console.log(`${strike} 스트라이크, ${ball} 볼 \n`);
+
+  return true;
 };
 
 // input값 숫자로 변경 후 배열로 리턴
